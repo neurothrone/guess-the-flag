5d32bfdd516182972f852ed539e7a9cc0fd9d31c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-09-30 06:23:17 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Zaid Neurothrone on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
